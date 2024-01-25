2f4e6f70d0b35343f5f59decd3c14b7869d26a49
unknown <miggy.payumo26@gmail.com>
2024-01-25 14:07:46 +0800

    Code Finalized & Labeled

--- Exercise-3-printing_letters_by_even_numbers.py
+++ Exercise-3-printing_letters_by_even_numbers.py
@@ -4,13 +4,19 @@
 # 3. Break the code if the number of characters is less than 2
 # 4. Print the characters in an even index number pattern.
 
+while True:
+# Input system for the user.
+    character_input = input("Please enter any character: ")
+    character_length = len(character_input)
 
-character_input = input("Please enter any character: ")
-character_length = len(character_input)
+# Labels so that we can easier see which is which.
+    print("Your word: ", character_input)
+    print("------------------------------")
+    print("Your new word: ")
 
-print("Your word: ", character_input)
-print("------------------------------")
-print("Your new word: ")
+# For printing the word in even index numbers.
+    for i in range(0, character_length -1, 2):
+        print("Number {", i, "}:", character_input[i])
 
-for i in range(0, character_length -1, 2):
-    print("Number {", i, "}:", character_input[i])
+    break
+
