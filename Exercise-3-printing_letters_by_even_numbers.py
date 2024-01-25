dda782aea92b005003ac19eaf4d00acd21a62340
unknown <miggy.payumo26@gmail.com>
2024-01-25 13:46:26 +0800

    Code can now recieve input from the user

---
+++ Exercise-3-printing_letters_by_even_numbers.py
@@ -0,0 +1,10 @@
+# Pseudocode
+# 1. Ask the user to input any character
+# 2. Evaluate the characters inputed by the user
+# 3. Break the code if the number of characters is less than 2
+# 4. Print the characters in an even index number pattern.
+
+
+character_input = input("Please enter any character: ")
+if character_input < 4:
+    print("Invalid, please input more characters.")
